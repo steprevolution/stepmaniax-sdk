f85058e4990740119b754b9581c6c7ed6e818cbe
Glenn Maynard <glenn@steprevolution.com>
2019-10-06 02:43:15 -0500

    Remove unused AllocConsole binding.
    This doesn't cause problems since it's never called, but it's never called.

--- smx-config/App.xaml.cs
+++ smx-config/App.xaml.cs
@@ -8,8 +8,6 @@ namespace smx_config
 {
     public partial class App: Application
     {
-        [DllImport("Kernel32")]
-        private static extern void AllocConsole();
         [DllImport("SMX.dll", CallingConvention = CallingConvention.Cdecl)]
         private static extern void SMX_Internal_OpenConsole();
 
